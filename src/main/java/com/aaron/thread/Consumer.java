85838912291791575d2912a42d0d8b77203b4ef8
nowhatever <aaron_26@163.com>
2014-08-15 17:54:26 +0800

    some thread demos

---
+++ src/main/java/com/aaron/thread/Consumer.java
@@ -0,0 +1,15 @@
+package com.aaron.thread;
+
+import com.aaron.bo.Info;
+
+class Consumer implements Runnable{
+	private Info info = null ;
+	public Consumer(Info info){
+		this.info = info ;
+	}
+	public void run(){
+		for(int i=0;i<10;i++){
+			this.info.get() ;
+		}
+	}
+}
